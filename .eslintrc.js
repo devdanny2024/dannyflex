72df03b6a09b85c7e104c5cdfd4b4abf52008396
solvvitalik <solvvitalik@gmail.com>
2025-04-23 08:35:24 +0100

    Publish

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+// .eslintrc.js
+module.exports = {
+    extends: ['next', 'next/core-web-vitals'],
+    rules: {
+      // Disable or override rules here
+      'react-hooks/exhaustive-deps': 'off',
+      'no-console': 'off',
+      'react/jsx-key': 'off',
+    },
+  }
+  
